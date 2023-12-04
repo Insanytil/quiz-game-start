9a248e85a53eed9a29f95d3df689466cf107721c
Jeudredix <lucien.vervaeren@gmail.com>
2023-12-04 17:44:00 +0100

    Setting param in private

--- rsc/quiz_brain.py
+++ rsc/quiz_brain.py
@@ -19,19 +19,19 @@ class QuizBrain:
 
 
         """
-        self.q_number = 0
-        self.q_list = q_list
-        self.q_score = 0
-        self.music = music.Music()
+        self.__q_number = 0
+        self.__q_list = q_list
+        self.__q_score = 0
+        self.__music = music.Music()
 
     def next_question(self):
-        current_question = self.q_list[self.q_number]
+        current_question = self.__q_list[self.__q_number]
         answers = [html.unescape(current_question[self.CORRECT_ANSWER_TAG])]
         for i in current_question[self.INCORRECT_ANSWER_TAG]:
             answers.append(html.unescape(i))
         random.shuffle(answers)
 
-        print(f"Question {self.q_number + 1}: {html.unescape(current_question[self.QUESTION_TAG])}")
+        print(f"Question {self.__q_number + 1}: {html.unescape(current_question[self.QUESTION_TAG])}")
         for index, item in enumerate(answers):
             print(f"   {index + 1} : {item}\n")
         try:
@@ -41,52 +41,52 @@ class QuizBrain:
         except ValueError:
             print('Please enter valid number')
             return self.next_question()
-        self.q_number += 1
+        self.__q_number += 1
 
         return self.checking_answer(user_answer, answers, html.unescape(current_question[self.CORRECT_ANSWER_TAG]))
 
     def is_not_last_question(self) -> bool:
-        return len(self.q_list) > self.q_number
+        return len(self.__q_list) > self.__q_number
 
     def checking_answer(self, user_answer, answer, question_answer):
         if user_answer - 1 == answer.index(html.unescape(question_answer)):
             print('Correct.')
-            self.music.play_correct_sound()
-            self.q_score += 1
+            self.__music.play_correct_sound()
+            self.__q_score += 1
         else:
-            self.music.play_wrong_sound()
+            self.__music.play_wrong_sound()
             print('Wrong.')
 
         print(f"""
         The correct answer is {question_answer}.
-        Your actual score is {self.q_score}/{self.q_number}.
+        Your actual score is {self.__q_score}/{self.__q_number}.
         
         """)
 
     def show_final_score(self):
-        if self.q_score == self.q_number:
+        if self.__q_score == self.__q_number:
             print(f"""
-            You've completed the quizz with a score of {self.q_score}/{self.q_number}.
+            You've completed the quizz with a score of {self.__q_score}/{self.__q_number}.
             What a stunning performance ! Congratulations. """)
 
-        if self.q_number > self.q_score >= (self.q_number / 10)*8:
+        if self.__q_number > self.__q_score >= (self.__q_number / 10)*8:
             print(f"""
-            You've completed the quizz with a score of {self.q_score}/{self.q_number}.
+            You've completed the quizz with a score of {self.__q_score}/{self.__q_number}.
             It was almost perfect ! Congratulations. """)
 
-        if (self.q_number / 10) * 8 > self.q_score > (self.q_number / 10) * 5:
+        if (self.__q_number / 10) * 8 > self.__q_score > (self.__q_number / 10) * 5:
             print(f"""
-            You've completed the quizz with a score of {self.q_score}/{self.q_number}.
+            You've completed the quizz with a score of {self.__q_score}/{self.__q_number}.
             More than one question in two correct, if it were an exam you'd probably have passed !
             Congratulations. """)
 
-        if (self.q_number / 10) * 5 == self.q_score:
+        if (self.__q_number / 10) * 5 == self.__q_score:
             print(f"""
-            You've completed the quizz with a score of {self.q_score}/{self.q_number}.
+            You've completed the quizz with a score of {self.__q_score}/{self.__q_number}.
             Just the Half... Not good, not bad !
             Congratulations. """)
-        if (self.q_number / 2) > self.q_score:
+        if (self.__q_number / 2) > self.__q_score:
             print(f"""
-            You've completed the quizz with a score of {self.q_score}/{self.q_number}.
+            You've completed the quizz with a score of {self.__q_score}/{self.__q_number}.
             Ouch not even the half, you'll do better next time don't worry  !
             Congratulations. """)
--- rsc_song/music.py
+++ rsc_song/music.py
@@ -10,16 +10,16 @@ class Music:
         PRE :
         POST : Initialise la class avec les chemins des fichiers audio depuis la racine du fichier main
         """
-        self.correct_song_path = "rsc_song/correct-6033.mp3"
-        self.wrong_song_path = "rsc_song/negative_beeps-6008.mp3"
-        self.volume = 0.2
+        self.__correct_song_path = "rsc_song/correct-6033.mp3"
+        self.__wrong_song_path = "rsc_song/negative_beeps-6008.mp3"
+        self.__volume = 0.2
 
     def play_wrong_sound(self):
         pygame.init()
         pygame.mixer.init()
 
-        sound = pygame.mixer.Sound(self.wrong_song_path)
-        sound.set_volume(self.volume)
+        sound = pygame.mixer.Sound(self.__wrong_song_path)
+        sound.set_volume(self.__volume)
         sound.play()
         pygame.time.delay(int(sound.get_length() * 1000))
 
@@ -27,7 +27,7 @@ class Music:
         pygame.init()
         pygame.mixer.init()
 
-        sound = pygame.mixer.Sound(self.correct_song_path)
-        sound.set_volume(self.volume)
+        sound = pygame.mixer.Sound(self.__correct_song_path)
+        sound.set_volume(self.__volume)
         sound.play()
         pygame.time.delay(int(sound.get_length() * 1000))
